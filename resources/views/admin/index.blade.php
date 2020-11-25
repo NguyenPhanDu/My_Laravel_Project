56deced70722e1dd724fdd53b6ebfc0d5a1f82cc
Du Nguyễn <nguyenphanduu12@gmail.com>
2020-11-25 14:28:56 +0700

    Create project laravel

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,4 @@
+@extends('admin.shared.layout')
+@section('content')
+    <h1>Index</h1>
+@endsection
---
+++ resources/views/admin/shared/footer.blade.php
@@ -0,0 +1,14 @@
+<!-- ============================================================== -->
+    <!-- All Jquery -->
+    <!-- ============================================================== -->
+    <script src="{!! asset('admin/assets/plugins/jquery/dist/jquery.min.js') !!}"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="{!! asset('admin/assets/plugins/popper.js/dist/umd/popper.min.js') !!}"></script>
+    <script src="{!! asset('admin/assets/plugins/bootstrap/dist/js/bootstrap.min.js') !!}"></script>
+    <script src="{!! asset('admin/js/app-style-switcher.js') !!}"></script>
+    <!--Wave Effects -->
+    <script src="{!! asset('admin/js/waves.js') !!}"></script>
+    <!--Menu sidebar -->
+    <script src="{!! asset('admin/js/sidebarmenu.js') !!}"></script>
+    <!--Custom JavaScript -->
+    <script src="{!! asset('admin/js/custom.js') !!}"></script>
